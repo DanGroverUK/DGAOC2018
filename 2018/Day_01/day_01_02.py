9cfcda26ce0333bae4ee6ce090049f749415f5c8
Dan Grover <dan-grover@dan-grover.com>
2018-12-01 18:24:32 +0000

    Working day one, task two!

--- 2018/Day_01/day_01_02.py
+++ 2018/Day_01/day_01_02.py
@@ -1,33 +1,22 @@
-imp = []
+int_imp = []
 
 fp = "C:/git/DGAOC2018/2018/Day_01/input.txt"
 with open(fp, mode='r') as doc:
     for line in doc:
-        imp.append(line)
-
-# imp = ["-1", "-1", "-1"]
-int_imp = []
-for a in imp:
-    int_imp.append(int(a))
+        int_imp.append(int(line))
 
 total = 0
-all_totals = [0]
 index = 0
+all_totals = [0]
+
 while index < len(int_imp):
     total = total + int_imp[index]
     if total in all_totals:
         print(total)
         break
     all_totals.append(total)
-    # print(all_totals)
     index = index + 1
     if index == len(int_imp):
         index = 0
     if (len(all_totals) % 10000) == 0:
         print(len(all_totals))
-
-
-
-
-
-# print(total)
